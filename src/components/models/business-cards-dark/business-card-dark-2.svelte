34e6936f0d85d208197480b8417eceb4789f8827
Ilias ASSADKI <68815850+iassadki@users.noreply.github.com>
2024-03-18 23:19:27 +0100

    Update business-card-dark-2.svelte

--- src/components/models/business-cards-dark/business-card-dark-2.svelte
+++ src/components/models/business-cards-dark/business-card-dark-2.svelte
@@ -18,11 +18,9 @@
     let adress = localStorage.getItem("adress")
         ? localStorage.getItem("adress")
         : "Adress";
-    let job = localStorage.getItem("job")
-        ? localStorage.getItem("job")
-        : "Job";
-    let company = localStorage.getItem("societe")
-        ? localStorage.getItem("societe")
+    let job = localStorage.getItem("job") ? localStorage.getItem("job") : "Job";
+    let company = localStorage.getItem("company")
+        ? localStorage.getItem("company")
         : "Company";
 
     $: if (form) {
@@ -35,7 +33,7 @@
                 localStorage.setItem("phone", phone);
                 localStorage.setItem("adress", adress);
                 localStorage.setItem("job", job);
-                localStorage.setItem("societe", company);
+                localStorage.setItem("company", company);
             });
     }
 
@@ -50,7 +48,7 @@
 
         let opt = {
             margin: 1,
-            filename: "business-card-dark-1.pdf",
+            filename: "business-card-dark-2.pdf",
             image: { type: "jpeg", quality: 0.98 },
             html2canvas: { scale: 2 },
             jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
@@ -65,95 +63,117 @@
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
 />
 
-<h3>Carte modifiable</h3>
-<div id="cards">
-    <div class="card-front" id="card-front">
-        <div class="card-header">
-            <p id="company" contenteditable="true">{company}</p>
-            <h2 id="civility" contenteditable="true">{civility}</h2>
-            <h2 id="firstname" contenteditable="true">{firstname}</h2>
-            <h2 id="name" contenteditable="true">{name}</h2>
+<h3>Business Card Dark 2</h3>
+<div class="container">
+    <div id="cards">
+        <div class="card-front" id="card-front">
+            <div class="card-header">
+                <p id="company" contenteditable="true">{company}</p>
+                <h2 id="civility" contenteditable="true">{civility}</h2>
+                <h2 id="firstname" contenteditable="true">{firstname}</h2>
+                <h2 id="name" contenteditable="true">{name}</h2>
+            </div>
+            <div class="card-body">
+                <div class="informations">
+                    <img alt="job" src="./src/assets/dark/case.png" />
+                    <p id="job" contenteditable="true">
+                        {job}
+                    </p>
+                </div>
+                <div class="informations">
+                    <img alt="email" src="./src/assets/dark/enveloppe.png" />
+                    <p id="email" contenteditable="true">
+                        {email}
+                    </p>
+                </div>
+
+                <div class="informations">
+                    <img alt="adress" src="./src/assets/dark/location.png" />
+                    <p id="adress" contenteditable="true">
+                        {adress}
+                    </p>
+                </div>
+
+                <div class="informations">
+                    <img alt="phone" src="./src/assets/dark/telephone.png" />
+                    <p id="phone" contenteditable="true">
+                        {phone}
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-            <div class="informations">
-                <img alt="job" src="./src/assets/dark/case.png" />
-                <p id="job" contenteditable="true">
-                    {job}
-                </p>
-            </div>
-            <div class="informations">
-                <img alt="email" src="./src/assets/dark/enveloppe.png" />
-                <p id="email" contenteditable="true">
-                    {email}
-                </p>
-            </div>
-
-            <div class="informations">
-                <img alt="adress" src="./src/assets/dark/location.png" />
-                <p id="adress" contenteditable="true">
-                    {adress}
-                </p>
-            </div>
-
-            <div class="informations">
-                <img alt="phone" src="./src/assets/dark/telephone.png" />
-                <p id="phone" contenteditable="true">
-                    {phone}
-                </p>
+        <div class="card-back" id="card-back">
+            <div class="card-header">
+                <p id="company" contenteditable="true">{company}</p>
             </div>
         </div>
     </div>
-    <div class="card-back" id="card-back">
-        <div class="card-header">
-            <p id="company" contenteditable="true">{company}</p>
-        </div>
+
+    <div class="form-container">
+        <form bind:this={form} id="carteForm">
+            <label for="name">Name</label>
+            <input
+                type="text"
+                bind:value={name}
+                id="name"
+                name="name"
+                required
+            />
+
+            <label for="firstname">Firstname</label>
+            <input
+                type="text"
+                bind:value={firstname}
+                id="firstname"
+                name="firstname"
+                required
+            />
+
+            <label for="email">Email</label>
+            <input
+                type="email"
+                bind:value={email}
+                id="email"
+                name="email"
+                required
+            />
+
+            <label for="phone">Phone</label>
+            <input
+                type="text"
+                bind:value={phone}
+                id="phone"
+                name="phone"
+                required
+            />
+
+            <label for="adress">Adress</label>
+            <input
+                type="text"
+                bind:value={adress}
+                id="adress"
+                name="adress"
+                required
+            />
+
+            <label for="job">Job</label>
+            <input type="text" bind:value={job} id="job" name="job" required />
+
+            <label for="company">Company</label>
+            <input
+                type="text"
+                bind:value={company}
+                id="company"
+                name="company"
+                required
+            />
+        </form>
+
+        <button type="submit" class="btn btn-primary" on:click={generatePDF3}
+            >Generate Card</button
+        >
     </div>
 </div>
-
-<form bind:this={form} id="carteForm">
-    <label for="nom">Nom:</label>
-    <input type="text" bind:value={name} id="nom" name="nom" required />
-
-    <label for="firstname">Prénom:</label>
-    <input type="text" bind:value={firstname} id="firstname" name="firstname" required />
-
-    <label for="email">Email:</label>
-    <input type="email" bind:value={email} id="email" name="email" required />
-
-    <label for="phone">phone:</label>
-    <input type="text" bind:value={phone} id="phone" name="phone" required />
-
-    <label for="adress">adress:</label>
-    <input
-        type="text"
-        bind:value={adress}
-        id="adress"
-        name="adress"
-        required
-    />
-
-    <label for="job">job:</label>
-    <input
-        type="text"
-        bind:value={job}
-        id="job"
-        name="job"
-        required
-    />
-
-    <label for="societe">Société:</label>
-    <input
-        type="text"
-        bind:value={company}
-        id="societe"
-        name="societe"
-        required
-    />
-</form>
-
-<button type="submit" class="btn btn-primary" on:click={generatePDF3}
-    >Générer CARTE</button
->
 
 <style>
     .informations {
@@ -170,8 +190,7 @@
     .informations p {
         margin: 10px 10px 10px 15px;
     }
-    
-    /* Card Front */
+
     .card-front .card-header {
         display: flex;
         justify-content: space-between;
@@ -187,6 +206,7 @@
         justify-content: center;
         flex-wrap: wrap;
     }
+
     .card-front {
         background-color: #000;
         border-radius: 5px;
@@ -197,7 +217,6 @@
         height: 300px;
     }
 
-    /* Card Back */
     .card-back {
         background-color: #000;
         border-radius: 5px;
@@ -214,5 +233,45 @@
         align-items: center;
         margin: 50px 0;
         font-size: 50px;
+    }
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        margin-bottom: 30px; 
+    }
+
+    #cards {
+        width: 500px; 
+    }
+
+    #carteForm label,
+    #carteForm input {
+        display: block;
+        margin-bottom: 10px;
+    }
+
+    #carteForm input[type="text"],
+    #carteForm input[type="email"] {
+        width: 95%;
+        padding: 10px;
+    }
+
+    .form-container {
+        width: 500px; 
+        padding: 20px;
+        background-color: #808080; 
+        border: solid 2px #000; 
+        border-radius: 30px;
+        margin-top: 15px;
+        margin-left: 100px;
+    }
+
+    .btn {
+        width: 100%;
+        margin-top: 20px;
+        background-color: #000;
+        color: #fff;
     }
 </style>
